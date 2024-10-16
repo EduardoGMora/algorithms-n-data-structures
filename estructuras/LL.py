a25930e69d7a12d2e89eb959edadf933626087fc
Eduardo Mora <eduardo@toolsnet.mx>
2024-10-16 15:20:16 -0600

    Add .gitignore file to ignore __pycache__/ directory
    se hicieron los métodos de la estructura de lista ligada dinámica

--- estructuras/LL.py
+++ estructuras/LL.py
@@ -8,14 +8,81 @@ class LL:
         self.head = None
         self.tail = None
 
+    def search(self, value):
+        temp = self.head
+        while temp is not None:
+            if temp.value == value:
+                return True
+            temp = temp.next
+        return False
+
     def agregartail(self, value):
-        pass
         new_node = node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
     
     def agregarhead(self, value):
-        pass
         new_node = node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
+    def agregarpos(self, value, pos):
+        if self.head is None:
+            return None
         
+        if pos == 0:
+            self.agregarhead(value)
+        elif pos >= self.contar():
+            self.agregartail(value)
+        else:
+            pre = self.head
+            for i in range(pos):
+                pre = pre.next
+                if pre is None:
+                    return None
+
+            aft = pre.next
+            new_node = node(value)
+            new_node.next = aft
+            pre.next = new_node
+
+    def eliminarhead(self):
+        if self.head is None:
+            return None
+
+    def contar(self):
+        temp = self.head
+        contador = 0
+        while temp is not None:
+            contador += 1
+            temp = temp.next
+        return contador
+    
+    def imprimir(self):
+        temp = self.head
+        while temp is not None:
+            print(f'{temp.value}', end=' -> ')
+            temp = temp.next
+
 
 if __name__ == '__main__':
     ll = LL()
+    ll.agregartail(1)
+    ll.agregartail(2)
+    ll.agregartail(3)
+    ll.agregarhead(0)
+    ll.agregarhead(-1)
+    ll.imprimir()
+    print()
+    print(ll.search(5))
+    ll.agregarpos(4, 20)
+    ll.imprimir()
+    print(f'\n{ll.contar()} elementos')
